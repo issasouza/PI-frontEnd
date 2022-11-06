7040ba3f913baf95c5947641ad7408f936fa7a65
Nataly <natalycarvalho404@gmail>
2022-11-06 09:18:21 -0300

    rotas e model

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CadastrarComponent } from './cadastrar/cadastrar.component';
+import { EntrarComponent } from './entrar/entrar.component';
+import { LadingPageComponent } from './lading-page/lading-page.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+
+  {path: "", redirectTo: 'entrar', pathMatch:'full'},
+
+  {path:'lading-page', component: LadingPageComponent},
+  {path:'entrar',component:EntrarComponent},
+  {path:'cadastrar', component: CadastrarComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ src/app/model/User.ts
@@ -0,0 +1,10 @@
+import { Produtos } from "./Produtos"
+export class User{
+    public id: number
+    public nome: string
+    public usuario: string
+    public senha: string
+    public foto: string
+    public tipo: string
+    public produtos: Produtos[]
+}
