a57c38841a66c632c95e153a6469e68268272c1b
Nataly <natalycarvalho404@gmail>
2022-11-06 08:19:34 -0300

    lading-page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,7 @@ import { MenuComponent } from './menu/menu.component';
 import { RodapeComponent } from './rodape/rodape.component';
 import { EntrarComponent } from './entrar/entrar.component';
 import { CadastrarComponent } from './cadastrar/cadastrar.component';
+import { LadingPageComponent } from './lading-page/lading-page.component';
 
 @NgModule({
   declarations: [
@@ -14,7 +15,8 @@ import { CadastrarComponent } from './cadastrar/cadastrar.component';
     MenuComponent,
     RodapeComponent,
     EntrarComponent,
-    CadastrarComponent
+    CadastrarComponent,
+    LadingPageComponent
   ],
   imports: [
     BrowserModule,
