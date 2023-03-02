6fa707549766d3c975f1470696422e160ffc60fc
Isabelly Souza <isabellysouza1998@gmail.com>
2023-03-02 17:01:33 -0300

    adicionando campos no login

--- src/app/model/User.ts
+++ src/app/model/User.ts
@@ -2,9 +2,18 @@ import { Produtos } from "./Produtos"
 export class User{
     public id: number
     public nome: string
+    public sobrenome: string
     public usuario: string
     public senha: string
+    public token: string
     public foto: string
     public tipo: string
+    public numero: string
+    public telefone: string
+    public cep: string
+    public bairro: string
+    public cpf: string
+    public complemento: string
+    public rua: string
     public produtos: Produtos[]
 }
--- src/app/model/UserLogin.ts
+++ src/app/model/UserLogin.ts
@@ -1,9 +1,18 @@
 export class UserLogin{
     public id: number
     public nome: string
+    public sobrenome: string
     public usuario: string
     public senha: string
     public token: string
     public foto: string
     public tipo: string
+    public numero: string
+    public telefone: string
+    public cep: string
+    public bairro: string
+    public cpf: string
+    public complemento: string
+    public rua: string
 }
+
