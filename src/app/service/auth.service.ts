2d1161cd25fef758379902f2a2b9eccb4de2daa7
Isabelly Souza <101573937+issasouza@users.noreply.github.com>
2023-05-23 15:57:38 -0300

    Update auth.service.ts

--- src/app/service/auth.service.ts
+++ src/app/service/auth.service.ts
@@ -11,23 +11,23 @@ import { UserLogin } from '../model/UserLogin';
 export class AuthService {
 
   constructor(
-    private http: HttpClient
+    private http: HttpClient 
   ) { }
 
   entrar(userLogin: UserLogin): Observable<UserLogin>{
-    return this.http.post<UserLogin>('http://localhost:8080/usuarios/logar', userLogin)
+    return this.http.post<UserLogin>('https://upvzinho-88f0.onrender.com/usuarios/logar', userLogin)
   }
   cadastrar(user: User):Observable<User>{
-    return this.http.post<User>('http://localhost:8080/usuarios/cadastrar', user)
+    return this.http.post<User>('https://upvzinho-88f0.onrender.com/usuarios/cadastrar', user)
   }
   atualizar(user:User): Observable<User>{
-    return this.http.put<User>('http://localhost:8080/usuarios/atualizar', user)
+    return this.http.put<User>('https://upvzinho-88f0.onrender.com/usuarios/atualizar', user)
   }
   getByIdUser(id:number):Observable<User>{
-    return this.http.get<User>(`http://localhost:8080/usuarios/${id}`)
+    return this.http.get<User>(`https://upvzinho-88f0.onrender.com/usuarios/${id}`)
   }
  findByUsuario(usuario:String): Observable<User>{
-    return this.http.get<User>(`http://localhost:8080/usuarios/esquecisenha/${usuario}`)
+    return this.http.get<User>(`https://upvzinho-88f0.onrender.com/usuarios/esquecisenha/${usuario}`)
   }
 
   logado(){
